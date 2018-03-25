f231a027ae90a6bbaf07fd9594e824af04f1c1dd
Anduin2017 <Anduin@aiursoft.com>
2018-03-25 13:01:44 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Kahla.Home
+
+[![Build Status](https://travis-ci.org/AiursoftWeb/Kahla.Home.svg?branch=master)](https://travis-ci.org/AiursoftWeb/Kahla.Home)
+
+The home page application for Kahla.
---
+++ src/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Aiursoft.Pylon;
+
+namespace Kahla.Home
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseEnforceHttps();
+            }
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseLanguageSwitcher();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
